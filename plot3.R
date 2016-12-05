33ed524b1853503c72e85b51e4874c4fca89ae07
JonJagd <jon-jagd@bueogpil.dk>
2016-12-05 20:11:54 +0100

    Nu virker plottet

--- plot3.R
+++ plot3.R
@@ -29,7 +29,9 @@ rm(list=ls())
 setwd("C:/Git/R/ExData_Plotting2")
 
 # Load required packages
-library(ggplot2)
+library(ggplot2) ## For creating ggplots
+install.packages("reshape")
+library(reshape) ## For manipulating data sets
 ##library(ggplot2movies)
 
 ## -----------------------------------------------------------------------------------
@@ -52,8 +54,16 @@ bcNonRoad <- subset.data.frame(NEI, NEI$fips == "24510" & NEI$type == "NON-ROAD"
 ## Summing up total emissions by year and type and putting it into a data frame
 ## -----------------------------------------------------------------------------------
 
+## tapply to calculate the total sum of PM2.5 per year and emision type
 totalBcAll <-with(bcAll, tapply(Emissions, INDEX = list(year, type), sum, na.rm = T))
+## Converting the matrix to a data frame
 totalBcAll <- as.data.frame(totalBcAll)
+## Adding year as a column to the data frame
+totalBcAll$Year <- row.names(totalBcAll)
+## Pivoting the columns for different types with PM2.5 data out to seperate rows
+totalBcAll <- melt(totalBcAll, id=c("Year"))
+## Renaming column names
+names(totalBcAll) <- c("Year", "Type", "Emissions")
 
 ## First we do it for type POINT
 ##totalBcPoint <-with(bcPoint, tapply(Emissions, year, sum, na.rm = T))
@@ -72,10 +82,10 @@ totalBcAll <- as.data.frame(totalBcAll)
 
 ##qplot(row.names(totalBcPoint), totalBcPoint, data=totalBcPoint, geom=c("point","smooth"), method="lm")
 
-ggplot(totalBcAll, aes(factor(row.names(totalBcAll)), totalBcAll)) +
+ggplot(totalBcAll, aes(Year, Emissions)) +
 geom_point() +
-facet_wrap(~names(totalBcAll), nrow = 2, ncol = 2) + ## Jeg mangler en header der hedder Type og en der hedder Sum og så skal det ligge i en tabel med 2 kolonner
+facet_grid(. ~Type) + ## Jeg mangler en header der hedder Type og en der hedder Sum og så skal det ligge i en tabel med 2 kolonner
 geom_smooth(method = "lm", se = FALSE, color = "black", aes(group = 1))
 
 
-dev.off()
+##dev.off()
